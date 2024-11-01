27fd44a0339561bc17f24c2cff05012275965e67
Vaibhav Pratap Singh <vaibhavpsingh179@gmail.com>
2024-11-01 15:54:03 +0530

    upd server.js

--- server.js
+++ server.js
@@ -15,12 +15,10 @@ app.use((req, res, next) => {
 });
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.log("Error connecting to MongoDB:", err));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("Error connecting to MongoDB:", err));
+
 
 // Root route
 app.get('/', (req, res) => {
