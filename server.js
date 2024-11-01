35d376217f5f8f335647d611cdab244f5e544b7f
Vaibhav Pratap Singh <vaibhavpsingh179@gmail.com>
2024-11-01 16:21:40 +0530

    port changed

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.post('/comments', async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
