d8723e17336569833ae7acccde4174814ebd378f
Vaibhav Pratap Singh <vaibhavpsingh179@gmail.com>
2024-11-01 16:48:02 +0530

    cors added in server.js :wq

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const Comment = require('./models/Comment'); // Ensure this path is correct
+const cors = require('cors');
+app.use(cors());
 
 const app = express();
 
