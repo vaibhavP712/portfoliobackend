45b23d53559b7fff21d68c47debd3c98f20ea984
Vaibhav Pratap Singh <vaibhavpsingh179@gmail.com>
2024-12-21 00:14:50 +0530

    modified cors

--- server.js
+++ server.js
@@ -12,17 +12,18 @@ const app = express();
 
 // Middleware
 app.use(helmet());
-const allowedOrigins = ['http://localhost:3000']; // Update with your frontend domain
-app.use(cors({
-    origin: function(origin, callback){
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            const msg = 'The CORS policy for this site does not allow access from the specified origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
+//const allowedOrigins = ['http://localhost:3000']; // Update with your frontend domain
+// app.use(cors({
+//     origin: function(origin, callback){
+//         if(!origin) return callback(null, true);
+//         if(allowedOrigins.indexOf(origin) === -1){
+//             const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+//             return callback(new Error(msg), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
+app.use(cors());
 
 app.use(morgan('combined'));
 
