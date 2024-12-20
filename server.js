fba38bde2757123695527f2f360e08ff2a16d0ee
Vaibhav Pratap Singh <vaibhavpsingh179@gmail.com>
2024-12-21 00:29:48 +0530

    logging middleware modified

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 // Logging middleware(optional)
 app.use((req, res, next) => {
-    console.log(`Request URL: ${req.url}`);
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
 });
 
